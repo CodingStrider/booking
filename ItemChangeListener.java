c2586012d66ccd609360a0ded30b58fb3ed567be
CodingStrider <83878600+CodingStrider@users.noreply.github.com>
2021-05-11 17:54:21 +0300

    aa
    a

--- ItemChangeListener.java
+++ ItemChangeListener.java
@@ -2,7 +2,7 @@ import java.awt.event.ItemEvent;
 import java.awt.event.ItemListener;
 
 public class ItemChangeListener implements ItemListener{
-
+ /** o ye beybi**/
 	@Override
 	public void itemStateChanged(ItemEvent e) {
 		if (e.getStateChange() == ItemEvent.SELECTED) {
